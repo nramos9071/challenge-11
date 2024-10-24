fcf4e45337c91a620a7dcae58f8accb159841dd3
nramos9071 <nramos9071@gmail.com>
2024-10-24 00:02:39 -0500

    fixed server

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 const express = require('express');
+const path = require('path');
+const routes = require('./routes/homeRoutes');
+
 const app = express();
+
+
+const PORT = 3001;
+
+// app.use('routes');
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
+
+
+
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+)
