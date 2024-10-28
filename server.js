0459393d9e7eacd484cdcf2db4a6f73b89851731
nramos9071 <nramos9071@gmail.com>
2024-10-28 17:43:37 -0500

    updates

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 // const routes = require('./routes/noteRoutes');
 const noteRoutes = require('./routes/noteRoutes');
+const indexRoutes = require('./routes/index');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const api = require('./routes/index');
 
 app.use(express.json());
 app.use('/api', noteRoutes)
+app.use('api', indexRoutes)
 
 
 
